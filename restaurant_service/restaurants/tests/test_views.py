8cbf7974e6dbda10aeb757299a2f0f2329127a22
Abraao Alcantara <sephioh@gmail.com>
2018-05-16 11:07:22 -0300

    Refactoring tests to its own modules

--- restaurant_service/restaurants/tests/test_views.py
+++ restaurant_service/restaurants/tests/test_views.py
@@ -1,44 +1,11 @@
 import json
 from datetime import time
 
-from django.db import IntegrityError
 from django.test import TestCase
 from model_mommy import mommy
 from rest_framework import status
 
 from restaurants.models import Restaurant
-
-
-class TestRestaurantModel(TestCase):
-    def test_create_a_restaurant(self):
-        Restaurant.objects.create(
-            name="The Incredible Restaurant",
-            opens_at=time(hour=10),
-            closes_at=time(hour=22)
-        )
-
-        self.assertEqual(1, Restaurant.objects.count())
-
-    def test_restaurants_must_have_a_name(self):
-        with self.assertRaises(IntegrityError):
-            Restaurant.objects.create(
-                opens_at=time(hour=10),
-                closes_at=time(hour=22)
-            )
-
-    def test_restaurants_must_have_a_opening_time(self):
-        with self.assertRaises(IntegrityError):
-            Restaurant.objects.create(
-                name="The Incredible Restaurant",
-                closes_at=time(hour=22)
-            )
-
-    def test_restaurants_must_have_a_closing_time(self):
-        with self.assertRaises(IntegrityError):
-            Restaurant.objects.create(
-                name="The Incredible Restaurant",
-                opens_at=time(hour=10),
-            )
 
 
 class TestRestaurantListCreateAPIView(TestCase):
